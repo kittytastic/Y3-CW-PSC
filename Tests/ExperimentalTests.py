b9be39a3246aa38b27b935fa4b46919d28a73d0a
Jack <jack@theslingsbys.com>
2020-12-18 14:46:44 +0000

    Fixed Experimental test

--- Tests/ExperimentalTests.py
+++ Tests/ExperimentalTests.py
@@ -8,8 +8,8 @@ def TwoBodyOrbit():
     init_bodies = [Body((1,0,0), (0,0.5,0), 2.0), Body((-1,0,0), (0,-0.5,0), 2.0)]
     sim_setup = SimArgs(snap_shot_t, final_t, dt, init_bodies)
 
-    final_bodies = [(1.12599, 0.231844, 0), (-1.12836, -0.231802, 0)]
-    expected_res = SimSolution(len(final_bodies), final_bodies, float(final_t)/float(dt), v_max=0.400216477567672, dx_min=2.30153727866805)
+    final_bodies = [(1.06395, -0.0442575, 0), (-1.06647, 0.04427, 0)]
+    expected_res = SimSolution(len(final_bodies), final_bodies, float(final_t)/float(dt), v_max=0.478111503159813, dx_min=2.13225348945739)
 
     return {"setup": sim_setup, "expected":expected_res}
     
--- Tests/TestSuite.py
+++ Tests/TestSuite.py
@@ -16,7 +16,7 @@ testSuites = {
     "full": [MoveRightTest, ShortMoveDiagTest, StationaryColide, TwoBodyOrbitTest, StandingCubeColide],
     "live": [ShortMoveDiagTest, StationaryColide],
     "collide": [StandingCubeColide],
-    "gen": [StandingCubeColide]
+    "gen": [TwoBodyOrbitTest]
 }
 
 def compile(file_name, destination="test.out", compiler_call="g++ -O3"):
